f9769bcfa84de2014da0eecae3439a4b3136f57c
Anna <atsigip@gmail.com>
2021-11-28 19:19:10 +0300

    even numbers from 1 to N

---
+++ Pr008/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать четные числа от 1 до N
+// определяем N
+// счетчик +1
+// четное число определяем еленем с остатком на 2
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int count = 1;
+
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
+
