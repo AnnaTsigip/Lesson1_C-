e20e7842bfce5824ab7426434d70beec4d6cbc26
Anna <atsigip@gmail.com>
2021-11-28 19:40:59 +0300

    the last digit of a three-digit number

---
+++ Pr009/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать последнюю цифру трёхзначного числа
+// 1. Ввести число
+// 2. проверить сколько в нем цифр
+// 3. найти третью цифру делением на 10 с статком
+
+Console.WriteLine("Введите трехзначное число: ");
+int threeDigitNumber = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if (threeDigitNumber >= 100 && threeDigitNumber <= 999)
+{
+    int thirdNumber = threeDigitNumber % 10;
+    Console.WriteLine(thirdNumber);
+}
+
+else 
+Console.WriteLine("Неправильное число символов");
+
