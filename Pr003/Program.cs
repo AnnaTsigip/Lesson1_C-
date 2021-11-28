63ba00f6f5553b9f0c7ecddbe150f89ac4ae5f3e
Anna <atsigip@gmail.com>
2021-11-28 17:53:23 +0300

    Pr003 day of the week by number

---
+++ Pr003/Program.cs
@@ -0,0 +1,37 @@
+﻿// // По заданному номеру дня недели вывести его название
+
+int dayOfWeek = 2;
+if (dayOfWeek == 1)
+{
+    Console.WriteLine("Понедельник");
+
+} 
+else if (dayOfWeek == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+else if (dayOfWeek == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (dayOfWeek == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (dayOfWeek == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (dayOfWeek == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (dayOfWeek == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Мф не знаем такого значения");
+}
