cf95b1a958bb8ea1624552b1fbe03e76a02a5101
Anna <atsigip@gmail.com>
2021-11-28 18:15:49 +0300

    Pr004 maximum of three

---
+++ Pr004/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти максимальное из трех чисел
+int a = 1;
+int b = 5;
+int c = 3;
+
+int max = a;
+
+if (a < b)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+
+if (max < c)
+{
+    max = c;
+}
+
+Console.WriteLine(max);
