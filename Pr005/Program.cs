1e1cdf3842bad9825ad635603574f83c4d33e234
Anna <atsigip@gmail.com>
2021-11-28 18:24:54 +0300

    function value

---
+++ Pr005/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+// y = x -1, x>=0
+// y= |x|, x < 0
+
+int x = -1;
+
+if (x >= 0)
+{
+    Console.WriteLine(x - 1);
+}
+else 
+{
+    Console.WriteLine(Math.Abs(x));
+}
