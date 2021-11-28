2c43ccdcfda6b525fa613ae9aebdcfc651a7f932
Anna <atsigip@gmail.com>
2021-11-28 18:32:47 +0300

    even number

---
+++ Pr006/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+int lastTaskValue = 6;
+
+if (lastTaskValue % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
