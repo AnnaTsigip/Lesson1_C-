54a9962d53c90472d89a70ce59906384318f0a09
Anna <atsigip@gmail.com>
2021-11-28 17:35:13 +0300

    Pr002 The greater and the lesser of the two

---
+++ Pr002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+// вводим 2 числа int
+
+int thirdTaskFirstValue = 9;
+int thirdTaskSecondValue = 10;
+
+if (thirdTaskFirstValue > thirdTaskSecondValue)
+{
+    Console.WriteLine("Большее " + thirdTaskFirstValue);
+    Console.WriteLine("Меньшее " + thirdTaskSecondValue);
+}
+ else if (thirdTaskFirstValue < thirdTaskSecondValue)
+ {
+    Console.WriteLine("Большее " + thirdTaskSecondValue);
+    Console.WriteLine("Меньшее " + thirdTaskFirstValue);  
+ }
+else
+{
+    Console.WriteLine("Числа равны!");
+}
