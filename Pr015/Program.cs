8d509d30e874a4c17559c7a2587706ce7bac2112
Anna <atsigip@gmail.com>
2021-11-29 18:43:24 +0300

    multiple of 7 and 23

---
+++ Pr015/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+// число кратно числу, если делится без остатка, т.е. если число % на 7 и 23 = 0
+// вводим число, делим на 7 и 23 % на 7 и 23 = 0, кратно, если % 7 и 23 > 0, некратно
+
+Console.WriteLine("Введите число для проверки: ");
+int numberToChek = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if(numberToChek % 7 == 0)
+{
+    Console.WriteLine("Число кратно 7");
+}
+else
+{
+    Console.WriteLine("Число не кратно 7");
+}
+
+if (numberToChek % 23 == 0)
+{
+    Console.WriteLine("Число кратно 23");
+}
+else
+{
+    Console.WriteLine("Число не кратно 23");
+}
