c90b22a28baa0130c191e0f27c66ae8d7b3f776c
Anna <atsigip@gmail.com>
2021-11-29 18:29:17 +0300

    third digit

---
+++ Pr014/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+// ввести число
+// третья цифра определяется делением %100
+
+Console.WriteLine("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if (number1 < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+if (number1 >= 100 && number1 <= 999)
+{
+ int thirdDigit = number1 / 100;
+ Console.WriteLine("Третье число: ");
+ Console.WriteLine(thirdDigit);
+}
+
+if (number1 > 999)
+{
+    int cutNumber = number1 % 1000;
+    int thirdDigit = cutNumber / 100;
+    Console.WriteLine("Третье число: ");
+    Console.WriteLine(thirdDigit);
+}
