bca2bc8ccdcee2b277ea43f64f380548770e180b
Anna <atsigip@gmail.com>
2021-11-29 19:14:01 +0300

    day off

---
+++ Pr016/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+// будни 1-5, выходной 6-7
+// ввести число, проверить соответствие
+
+Console.WriteLine("Введите число от 1 до 7"); 
+int dayOfWeek = int.Parse(Console.ReadLine());
+Console.WriteLine();
+if (dayOfWeek < 6)
+{
+    Console.WriteLine("Будний день");
+}
+else
+{
+    Console.WriteLine("Выходной");
+}
