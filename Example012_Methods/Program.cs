9e61d70a095492608a252301103665e7c2f697e9
Anna <atsigip@gmail.com>
2021-12-06 21:48:57 +0300

    info For

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,57 @@
+﻿// вид 1 void ничего не возвращает
+// void Method1()
+// {
+//     Console.WriteLine("Афтор ...");
+// }
+
+// Method1(); //Вызвать метод
+
+
+// Вид 2 ничего не возвращает, но может принимать значения
+ 
+//  void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+  int i = 0;
+  while (i < count)
+  {
+      Console.WriteLine(msg);
+      i++;
+  }  
+}
+//Method21(msg: "Текст", count: 4);
+Method21(count: 4, msg: "новый текст");
+
+
+// вид3 что возвращает, но не принимает
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+//вид4 что-то принимают и что-то возвращают
+
+string Method4(int count, string текс)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + текс;
+        i++;
+    }
+return result;
+}
+string res = Method4(10, "Привет!");
+Console.WriteLine(res);
+
+
---
+++ Фунцции/Program.cs
@@ -0,0 +1,57 @@
+﻿// Функции
+// тип_данных_возвращаемый имя_метода(входной параметр1, входной параметр2, ..., входной параметрN)
+// {
+// из_набора_опреаторов_и_синтаксических_конструкций;    
+// }
+//метод Void - ничего не возвращает
+void myMethod()
+{
+    int a = 0;
+    int b = 5;
+    Console.WriteLine(a + b);
+}
+
+myMethod();
+
+//return  - возвращаемый метод
+
+
+int myMethod1()
+{
+    int a = 1;
+    int b = 5;
+    return a + b;
+}
+
+int c = myMethod1();
+Console.WriteLine(c);
+
+
+bool myMethod2()
+{
+    int a = 8;
+    int b = 7;
+    return a < b;
+}
+
+bool flag = myMethod2();
+Console.WriteLine(flag);
+
+
+//Входной параметр - тип_данных имя_переменной
+
+int myMethod3(int a, int b)
+{
+    return a + b; 
+}
+
+int k = myMethod3(2, 5);
+
+int x = 5;
+int y = 10;
+
+int z = myMethod3(x, y);
+Console.WriteLine(z);
+Console.WriteLine(k);
+
+
