89e21d21c93a162fae07a48bdbcec33594500b0d
Anna <atsigip@gmail.com>
2021-12-08 21:58:54 +0300

    Pr027 number of digits in a number

---
+++ Pr027/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить количество цифр в числе
+// ввести число
+// делить число на %10, посчитать сколько делений потребовалось?
+
+// int num = new Random().Next();
+// Console.WriteLine("Рандомное число: ");
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = 0;
+int num = 0;
+//Console.WriteLine(num);
+while (number > 0)
+{
+    num = number / 10;
+    //number = count;
+    digit++;
+
+}
+
+Console.WriteLine($"Данное число содержит {digit} цифр");
