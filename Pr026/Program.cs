825ccfb975985fbe9c86f877c6976383dd6cc3a9
Anna <atsigip@gmail.com>
2021-12-08 20:58:13 +0300

    Pr026 degree of number A in B

---
+++ Pr026/Program.cs
@@ -0,0 +1,19 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+// ввести А
+// ввести степень
+// Умножение А на А по числу B
+
+Console.WriteLine("Ведите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральную степень B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int degreeA = 1;
+for (double i = 1; i <= B; i++)
+{
+    
+  degreeA *= A;
+
+}
+//degreeA = Math.Pow(A, B); тогда double
+Console.WriteLine($"Число {A} в степени {B} = {degreeA}");
+
