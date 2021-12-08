833cef5cb1222e0d980c4fb4fbedb64af7eb0906
Anna <atsigip@gmail.com>
2021-12-08 19:07:26 +0300

    Pr024 cube number

--- Pr024/Program.cs
+++ Pr024/Program.cs
@@ -3,5 +3,12 @@
 // число возвести в куб - a*a*a
 
 
- 
+ Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int count = 1; count <= N; count++)
+{
+   int cubeNumber = count * count * count;
+   Console.WriteLine($"число {count} в кубе = {cubeNumber}");
+}
+
 
