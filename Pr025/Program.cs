e82b952078ac44f5db81a86d6942bc20a1e6596a
Anna <atsigip@gmail.com>
2021-12-08 19:19:55 +0300

    Pr025 summa numdet from 1 to A

---
+++ Pr025/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму чисел от 1 до А
+// вводим число А
+// переменную сумма, ддобавляем в сумму все числа от 1 до А
+
+Console.WriteLine("введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+for (int count = 1; count <= A; count++)
+{
+    summa = summa + count;
+    
+}
+Console.WriteLine($"сумма всех числел от 1 до {A} = {summa}");
