f3e49b0ab926d478ead674c2857aedc3fc8d18a5
Anna <atsigip@gmail.com>
2021-12-09 19:54:24 +0300

    Pr028 sum of digits in a number

---
+++ Pr028/Program.cs
@@ -0,0 +1,38 @@
+﻿// Подсчитать сумму цифр в числе
+// вводим число int
+// разложить число на цифры
+// складываем цифры числа
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = 0;
+int i = 0;
+while (N > 0)
+{
+
+    temp = temp + N % 10;
+    N = N / 10;
+    i++;
+}
+
+Console.WriteLine(temp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
