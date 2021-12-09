aa5103d003258051022f1af7d4ff20beb9a42367
Anna <atsigip@gmail.com>
2021-12-09 23:54:46 +0300

    Pt031 array of 8 elements

---
+++ Pr031/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int[] myarray = new int[8];
+myarray[0] = 1;
+int i = 0;
+while (i < myarray.Length)
+{
+    myarray[i] = i + 1;
+    i++;
+}
+
+void PrintArray (int[] array)
+{
+   int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(myarray);
